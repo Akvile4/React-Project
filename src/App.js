c38db8be32e7cfaacf6df1cf7c5b4e2ef14905e0
Akvile4 <aqua0818@gmail.com>
2022-02-21 16:48:11 +0200

    added postCreater component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+// import React from "react";
+import { useState } from 'react';
 import './App.css';
+import { PostCreator } from './components/postCreater';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState();
+  const [list, setList] = useState([]);
+  const [postContent, setPostContent] = useState();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (postContent) {
+        // spread content with ...
+      setList([...list, postContent])
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? <h1> Welcone {user} </h1> : <h1>Please log in</h1>}
+        {/* e is event, we need anonymous function to not to update constantly */}
+      <input onChange={(e) => setUser(e.target.value)} />
+      <PostCreator setPostContent={setPostContent} 
+      list={list} 
+      submitHandler={submitHandler} />
+      <div>
+          {/* takes individual item puts in array  */}
+          {/* key makes it specific  */}
+        {list.map((item, index) => 
+          <h2 key={index}>{item}</h2>)}
+      </div>
+      <p> I'm a react app </p>
     </div>
   );
 }
